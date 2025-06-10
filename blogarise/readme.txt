6bb757e62fdca73a1763b0ef8d3c452ff512adbd
Sean Murphy <sean@iamseanmurphy.com>
2025-06-10 01:47:36 +0000

    1.1.8

--- blogarise/readme.txt
+++ blogarise/readme.txt
@@ -4,8 +4,8 @@ Author: Themeansar
 Requires at least: WordPress 6.6
 Tested up to: 6.8
 Requires PHP: 7.4
-Stable tag: 1.1.7
-Version: 1.1.7
+Stable tag: 1.1.8
+Version: 1.1.8
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Tags: one-column, two-columns ,right-sidebar, flexible-header, custom-background, custom-header, custom-menu, editor-style, featured-images, footer-widgets,  theme-options, threaded-comments, rtl-language-support, translation-ready, full-width-template, custom-logo, blog, news
@@ -461,3 +461,6 @@ Added Keyword Control.
 
 = Version 1.1.7
 * Fixed Styling Issues.
+
+= Version 1.1.8
+* Added Image Resolution Class in Images.
--- blogarise/template-parts/content-page.php
+++ blogarise/template-parts/content-page.php
@@ -34,13 +34,13 @@ if( class_exists('woocommerce') && (is_account_page() || is_cart() || is_checkou
 							if(has_post_thumbnail()) {
 								if ( is_single() ) { ?>
 									<figure class="post-thumbnail">
-										<?php the_post_thumbnail('full'); ?>					
+										<?php the_post_thumbnail('full', array('class' => 'img-fluid attachment-full size-full')); ?>					
 									</figure>
 								<?php }
 								else { ?>
 									<figure class="post-thumbnail">
 										<a href="<?php the_permalink(); ?>" >
-											<?php the_post_thumbnail('full'); ?>
+											<?php the_post_thumbnail('full', array('class' => 'img-fluid attachment-full size-full')); ?>
 										</a>				
 									</figure>
 								<?php }
