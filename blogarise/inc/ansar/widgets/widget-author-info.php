ff39c8683ffc8e97fa79e082f586120cc5a2f052
Sean Murphy <sean@iamseanmurphy.com>
2025-09-22 03:45:27 +0000

    1.3.4

--- blogarise/inc/ansar/widgets/widget-author-info.php
+++ blogarise/inc/ansar/widgets/widget-author-info.php
@@ -22,6 +22,20 @@ class blogarise_author_info extends WP_Widget {
 
         extract($args);
 
+        $defaults = array(
+            'title'    => '',
+            'name'     => '',
+            'desc'     => '',
+            'image_uri'=> '',
+            'facebook' => '',
+            'twt'      => '',
+            'insta'    => '',
+            'youtube'  => '',
+            'open_btnone_new_window' => 0,
+        );
+        
+        $instance = wp_parse_args( (array) $instance, $defaults );
+
         echo $before_widget;
         
         $blogarise_btnone_target = '_self';
@@ -56,33 +70,43 @@ class blogarise_author_info extends WP_Widget {
         echo $after_widget;
 
     }
+
     function update($new_instance, $old_instance) {
-
         $instance = $old_instance;
-        $instance['facebook'] = stripslashes(wp_filter_post_kses($new_instance['facebook']));
-        $instance['open_btnone_new_window'] = strip_tags($new_instance['open_btnone_new_window']);
-        $instance['image_uri'] = strip_tags($new_instance['image_uri']);
-        $instance['title'] = strip_tags($new_instance['title']);
-        $instance['name'] = strip_tags($new_instance['name']);
-        $instance['desc'] = strip_tags($new_instance['desc']);
-        $instance['twt'] = stripslashes(wp_filter_post_kses($new_instance['twt']));
-        $instance['insta'] = stripslashes(wp_filter_post_kses($new_instance['insta']));
-        $instance['youtube'] = stripslashes(wp_filter_post_kses($new_instance['youtube']));
 
-        $businessup_btnone_target = '_self';
-        if( !empty($instance['open_btnone_new_window']) ):
-            $businessup_btnone_target = '_blank';
-        endif;
+        // Basic text fields
+        $instance['title'] = sanitize_text_field( $new_instance['title'] ?? '' );
+        $instance['name']  = sanitize_text_field( $new_instance['name'] ?? '' );
+
+        // Description - lets keep it plain text but use textarea sanitizer
+        $instance['desc'] = sanitize_textarea_field( $new_instance['desc'] ?? '' );
+
+        // Image URL and social URLs — sanitize as URLs
+        $instance['image_uri'] = esc_url_raw( $new_instance['image_uri'] ?? '' );
+        $instance['facebook']  = esc_url_raw( $new_instance['facebook'] ?? '' );
+        $instance['twt']       = esc_url_raw( $new_instance['twt'] ?? '' );
+        $instance['insta']     = esc_url_raw( $new_instance['insta'] ?? '' );
+        $instance['youtube']   = esc_url_raw( $new_instance['youtube'] ?? '' );
+
+        // Checkbox — ensure it's always 0 or 1 (avoids undefined index / null)
+        $instance['open_btnone_new_window'] = ! empty( $new_instance['open_btnone_new_window'] ) ? 1 : 0;
 
         return $instance;
-
     }
 
     function form($instance) {
-        $instance['title'] = (isset($instance['title'])?$instance['title']:'Author Details');
-        $instance['name'] = (isset($instance['name'])?$instance['name']:'Author');
-        $instance['desc'] = (isset($instance['desc'])?$instance['desc']:'');
-
+        $defaults = array(
+            'title' => 'Author Details',
+            'name'  => '',
+            'desc'  => '',
+            'image_uri' => '',
+            'facebook' => '',
+            'twt' => '',
+            'insta' => '',
+            'youtube' => '',
+            'open_btnone_new_window' => 0,
+        );
+        $instance = wp_parse_args( (array) $instance, $defaults );
         ?>
            <p>
           <label for="<?php echo esc_attr($this->get_field_id( 'title' )); ?>"><?php esc_html_e( 'Title','blogarise' ); ?></label> 
@@ -108,16 +132,16 @@ class blogarise_author_info extends WP_Widget {
         <p>
           <label for="<?php echo esc_attr($this->get_field_id( 'name' )); ?>"><?php esc_html_e( 'Name','blogarise' ); ?></label> 
           <input class="widefat" id="<?php echo esc_attr($this->get_field_id( 'name' )); ?>" name="<?php echo esc_attr($this->get_field_name( 'name' )); ?>" type="text" value="<?php echo esc_attr( $instance['name'] ); ?>" />
-          </p>
+        </p>
 
           <p>
-          <label for="<?php echo esc_attr($this->get_field_id( 'desc' )); ?>"><?php esc_html_e( 'Description','blogarise' ); ?></label> 
-          <input class="widefat" id="<?php echo esc_attr($this->get_field_id( 'desc' )); ?>" name="<?php echo esc_attr($this->get_field_name( 'desc' )); ?>" type="textarea" value="<?php echo esc_attr( $instance['desc'] ); ?>" />
+            <label for="<?php echo esc_attr($this->get_field_id( 'desc' )); ?>"><?php esc_html_e( 'Description','blogarise' ); ?></label> 
+           <textarea class="widefat" id="<?php echo esc_attr($this->get_field_id( 'desc' )); ?>" name="<?php echo esc_attr($this->get_field_name( 'desc' )); ?>"><?php echo esc_textarea( $instance['desc'] ); ?></textarea>
           </p>
       
             
         <table>
-      <tr>
+            <tr>
                 <td>
                     <label for="<?php echo esc_attr($this->get_field_id('facebook')); ?>"><?php esc_html_e('Facebook Link', 'blogarise'); ?></label>
                 </td>
@@ -160,13 +184,16 @@ class blogarise_author_info extends WP_Widget {
                     <input type="text" name="<?php echo esc_attr($this->get_field_name('youtube')); ?>" id="<?php echo esc_attr($this->get_field_id('youtube')); ?>" value="<?php if( !empty($instance['youtube']) ): echo esc_attr($instance['youtube']); endif; ?>" class="widefat"/>
                 </td>
             </tr>
-
-            <tr>
-                <td colspan="2">
-                    <input type="checkbox" name="<?php echo esc_attr($this->get_field_name('open_btnone_new_window')); ?>" id="<?php echo esc_attr($this->get_field_id('open_btnone_new_window')); ?>" <?php if( !empty($instance['open_btnone_new_window']) ): checked( (bool) $instance['open_btnone_new_window'], true ); endif; ?> ><?php esc_html_e( 'Open link in new tab','blogarise' ); ?>
-                </td>
-            </tr>
         </table>
+        <p>
+            <label for="<?php echo esc_attr($this->get_field_id('open_btnone_new_window')); ?>">
+                <input type="checkbox" 
+                    id="<?php echo esc_attr($this->get_field_id('open_btnone_new_window')); ?>"
+                    name="<?php echo esc_attr($this->get_field_name('open_btnone_new_window')); ?>"
+                    value="1" <?php checked( $instance['open_btnone_new_window'], 1 ); ?> />
+                <?php esc_html_e( 'Open link in new tab','blogarise' ); ?>
+            </label>
+        </p>
     <?php
 
     }
